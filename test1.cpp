f1eebff45236cc24bb0e6d5a3947ba16d374ebc9
HamDonggyun <80018275+Hameast@users.noreply.github.com>
2022-04-06 13:49:02 +0900

    Create test1.cpp
    test "do while" and "goto"

---
+++ test1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int cnt;
+  bool flag_1 = true;
+  
+  tag_test:
+  
+  cnt = 3;
+  
+  do
+  {
+    cout << cnt << "do while문과 goto문을 연습해 봅시다.\n";
+    cnt--;
+  }while(cnt);    //세미콜론 (;) 빼먹지 말 것!!
+
+  if(flag_1){
+    flag_1 = false;
+    goto tag_test;
+  }
+
+  
+  return 0;
+}
