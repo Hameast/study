a5b0d59c741c57a41081299d8f7cb1b31c004844
HamDonggyun <80018275+Hameast@users.noreply.github.com>
2022-10-01 11:20:02 +0900

    datascience

--- 22/DataSience/main.py
+++ 22/DataSience/main.py
@@ -1,9 +1,19 @@
+# main.py
+
+# 본 파일과 같은 디렉토리에 위치한 my_fuction을 사용한다.
 import my_function as mf
 
-
 if __name__ == '__main__':
-    li = list(map(int, input().split()))
+    # 사용자로부터 임의의 값 10개를 입력받아
+    # list로 저장
+    # li = list(map(int, input().split()))
+    print("10개의 숫자를 입력하세요")
+    li = []
+    for i in range(10):
+        li.append(int(input(f"{i + 1}. 숫자를 입력하세요 : ")))
 
-    print(mf.getMax(li))
-    print(mf.getMin(li))
-    print(mf.getAvg(li))
+    # my_function의 함수를 호출하여
+    # 최대값, 최소값, 평균값 출력
+    print(f"최대값은 : {mf.getMax(li)}")
+    print(f"최소값은 : {mf.getMin(li)}")
+    print(f"평균값은 : {mf.getAvg(li)}")
--- 22/DataSience/my_function.py
+++ 22/DataSience/my_function.py
@@ -1,5 +1,13 @@
+# my_function.py
+
+# 최대값을 구하는 함수입니다.
+# list형태의 변수를 매개변수로 한다.
+# 2중 for문을 돌며 list내의 변수들을 각각 비교한다
+# val보다 큰 값이 존재한다면 break하여 다음 val을 갖고 비교한다
+# val보다 큰 값이 존재하지 않는다면 break_flag가 false로 남아
+# max_val값을 저장하고 2중 포문에서 탈출한다.
 def getMax(li):
-    max_val = 0
+    max_val = li[0]
     for val in li:
         break_flag = False
         for val2 in li:
@@ -12,8 +20,10 @@ def getMax(li):
     return max_val
 
 
+# 최솟값을 구하는 함수입니다.
+# 위의 최대값을 구하는 함수에서 부등호의 방향만 바뀌었습니다.
 def getMin(li):
-    min_val = 0
+    min_val = li[0]
     for val in li:
         break_flag = False
         for val2 in li:
@@ -26,6 +36,10 @@ def getMin(li):
     return min_val
 
 
+# 평균값을 구하는 함수입니다.
+# len()함수를 사용해도 되지만, len_cnt를 두어 list의 길이를 저장했습니다.
+# for문을 돌며 list내의 val값들을 합산하고
+# 합산된 val_sum값을 len_cnt로 나눈 평균값을 반환합니다.
 def getAvg(li):
     len_cnt = 0
     val_sum = 0
